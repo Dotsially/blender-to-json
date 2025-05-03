4422a97c4fcdcc7359d4940a26068ef5fcef6472
Dotsially <mateu9999@gmail.com>
2025-05-03 15:21:00 +0200

    temp uv commit

--- blender_to_json.py
+++ blender_to_json.py
@@ -11,6 +11,7 @@ object = bpy.context.active_object
 
 if object and object.type == 'MESH':
     mesh = object.data
+    
     vertices = []
     indices = []
         
@@ -19,7 +20,19 @@ if object and object.type == 'MESH':
         vertices.append(v.co.z)
         vertices.append(v.co.y)
     
+    uv_layer = mesh.uv_layers.active.data
+    uv_coords = [None] * int((len(vertices)/3)*2)
+    
+    looked_at = {}
     for face in mesh.polygons:
+        for vertex_index in face.vertices:
+            if vertex_index in looked_at:
+                continue
+            looked_at[vertex_index] = 0
+            
+            uv_coords[vertex_index*2] = uv_layer[vertex_index].uv.x
+            uv_coords[vertex_index*2+1] = uv_layer[vertex_index].uv.y
+        
         if len(face.vertices) == 3:
             indices.extend([face.vertices[0], face.vertices[1], face.vertices[2]])
         elif len(face.vertices) > 3:
@@ -29,8 +42,12 @@ if object and object.type == 'MESH':
                 vertex_3 = face.vertices[i+2]
                 indices.extend([vertex_1, vertex_2, vertex_3])
     
+    print(len(vertices))
+    print(len(uv_coords))
+    
     data = {
         "vertices" : vertices,
+        "uv_coords" : uv_coords,
         "indices" : indices
     }
     
