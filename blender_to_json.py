44ba11783e2c52fbdad5cfecbac950e06fe2313e
Dotsially <mateu9999@gmail.com>
2025-05-04 15:19:40 +0200

    wrong commit

--- blender_to_json.py
+++ blender_to_json.py
@@ -13,42 +13,34 @@ if object and object.type == 'MESH':
     mesh = object.data
     
     vertices = []
-    uv_coords = []
-    indices = []
     
+    for v in mesh.vertices:
+        vertices.append(v.co.x)
+        vertices.append(v.co.z)
+        vertices.append(v.co.y)
+    
+    faces = []
     uv_layer = mesh.uv_layers.active.data
-    
-    index_count = 0
     for face in mesh.polygons:
+        uv_coords = []
+        indices = []
         
-        for loop_index, vertex_index in enumerate(face.vertices):
-            vertex = mesh.vertices[vertex_index]
-            vertices.append(vertex.co.x)
-            vertices.append(vertex.co.z)
-            vertices.append(vertex.co.y)
-            
+        for loop_index, vertex_index in enumerate(face.vertices):    
             uv_coord = uv_layer[face.loop_indices[loop_index]].uv
             uv_coords.extend([uv_coord.x, uv_coord.y])
             
-        if len(face.vertices) == 3:
-            indices.extend([face.vertices[0] + index_count, face.vertices[1] + index_count, face.vertices[2] + index_count])
-        elif len(face.vertices) > 3:
-            vertex_1 = face.vertices[0] + index_count
-            for i in range(len(face.vertices)-2):
-                vertex_2 = face.vertices[i+1] + index_count
-                vertex_3 = face.vertices[i+2] + index_count
-                indices.extend([vertex_1, vertex_2, vertex_3])
+            indices.append(vertex_index)
         
-        index_count += 4
-    
-    print(len(vertices))
-    print(len(indices))
-    print(len(vertices))
+        face = {
+            "uv_coords" : uv_coords,
+            "indices" : indices    
+        }
+        
+        faces.append(face)        
     
     data = {
         "vertices" : vertices,
-        "uv_coords" : uv_coords,
-        "indices" : indices
+        "faces" : faces
     }
     
     with open(output_file, 'w') as file:
